f4a9ea6dc3ea7fec723d9fd089bb476c03f92eab
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-27 16:43:07 +0100

    first testing

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
 function add(a, b) {
     return a + b
 }
+
+function toSnowflakeCase(str) {
+    return str
+        .split('')
+        .map((c, i) => (i % 2 === 0 ? c.toLowerCase() : c.toUpperCase()))
+        .join('')
+}
--- utils.test.js
+++ utils.test.js
@@ -4,4 +4,17 @@ describe('add() method', function() {
     it('should be a function', function() {
         expect(typeof add).toEqual('function')
     })
+
+    it('should return a number', function() {
+        const result = add(5, 1)
+        expect(typeof result).toEqual('number')
+    })
+
+    it('should add up two numbers', function() {
+        expect(add(2, 3)).toEqual(5)
+    })
+
+    it('should ignore additional arguments (more than two)', function() {
+        expect(add(6, 6, 6)).toEqual(add(6, 6))
+    })
 })
