b50c89786ea667385f2b38360e8f625f3ec85003
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-28 10:44:17 +0100

    gsap ball drop

---
+++ game.js
@@ -0,0 +1,17 @@
+// TweenMax.to('.ball', 1.5, { y: 500 })
+// TweenMax.staggerTo('.ball', 1.5, { y: 500 }, 0.25)
+// TweenMax.staggerFromTo(
+//     '.ball',
+//     1.5,
+//     { y: 0, scale: 0.1, opacity: 0 },
+//     { y: 500, scale: 1, opacity: 1, ease: Bounce.easeOut },
+//     0.25
+// )
+
+// TweenMax.staggerFromTo(
+//     '.ball',
+//     1.5,
+//     { y: 0, scale: 0.1, opacity: 0 },
+//     { y: 500, scale: 1, opacity: 1, ease: Bounce.easeOut, yoyo: true, repeat: -1 },
+//     0.25
+// )
